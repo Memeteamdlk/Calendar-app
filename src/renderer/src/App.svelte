531b04fa8d0a42715997f6ee39fd8c946114118c
Memeteamdlk <jasongerardi94@gmail.com>
2024-02-01 14:33:34 -0800

    Tool bar added

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,7 +1,62 @@
-<script lang="ts">
-  import Versions from './components/Versions.svelte'
-  import electronLogo from './assets/electron.svg'
+<script>
+  
 </script>
 
+<body>
+  <div id="container">
+    <nav>
+      <div id="titleShown"></div>
+      <div id="buttons">
+        <div id="minimize">
+            <span id="buttonspan">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M5 12h14"></path>
+              </svg>
+            </span>
+        </div>
+        <div id="maximize">
+            <span id="buttonspan">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path
+                  d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3M3 16v3a2 2 0 0 0 2 2h3m8 0h3a2 2 0 0 0 2-2v-3"
+                ></path>
+              </svg>
+            </span>
+        </div>
+        <div id="closeWindow">
+            <span id="buttonspan">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M18 6L6 18M6 6l12 12"></path>
+              </svg>
+            </span>
+        </div>
+      </div>
+    </nav>
+  </div>
 
 
+</body>
